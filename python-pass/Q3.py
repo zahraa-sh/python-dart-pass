b4422e1dce2280f620a03bda5ada996a3a7e0c41
zahraa-sh <103223965+zahraa-sh@users.noreply.github.com>
2022-04-09 16:24:51 +0300

    Update Q3.py

--- python-pass/Q3.py
+++ python-pass/Q3.py
@@ -1,1 +1,20 @@
+class myNumber:
+    def __init__(self):
+        pass
+    def checkNumbers(self):
+        n=[]
+        r = int(input("How many values do you want? "))
+        for i in range (0,r):
+          x= int(input(""))
+          n.append(x) 
 
+        for i in n:
+            
+            if ((i > 0) & (i <= 10)):
+                if (i % 2 == 0):
+                    print(i, " is even")
+                else:
+                    print(i, " is odd")
+
+m = myNumber()
+m.checkNumbers()
